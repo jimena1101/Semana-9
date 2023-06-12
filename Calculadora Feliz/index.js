b3b68fd6a768d098b7a23f7a25fe62a770f90acb
jimena1101 <jimenamedina_1994@hotmail.com>
2023-06-12 09:38:47 -0500

    cambios

--- Calculadora Feliz/index.js
+++ Calculadora Feliz/index.js
@@ -3,10 +3,33 @@ const result = document.getElementById('result');
 happyForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-
+    let value = 0 ;
     const number1 = +happyForm['number1'].value;
     const operator = happyForm['operator'].value;
     const number2 = +happyForm['number2'].value;
-    result.textContent = number1 + number2;
     
+    switch (operator) {
+        case '+':
+        value= number1 + number2;
+        break;
+
+        case '-':
+        value= number1 + number2;
+        break;
+            
+        case '*':
+        value= number1 + number2;
+         break;
+         case '/':
+         value= number1 + number2;
+         break;
+         default;
+         break;}
+         
+    
+    
+    
+    
+    result.textContent = number1 + number2;
+
 });
